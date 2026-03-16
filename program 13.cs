9ef2fbf8e7261ce214ad2519006a2181732ad8e5
kandregulapallavi <kandregula.ramkrishna127630@marwadiuniversity.ac.in>
2026-03-16 10:02:45 +0530

    Add files via upload

---
+++ program 13.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Rectangle
+{
+	double length;
+	double width;
+
+	public Rectangle()
+	{
+		length = 0;
+		width = 0;
+	}
+
+	public Rectangle(double l, double w)
+	{
+		length = l;
+		width = w;
+	}
+
+	public double FindArea()
+	{
+		return length * width;
+	}
+
+	public void Display()
+	{
+		Console.WriteLine("Length: " + length);
+		Console.WriteLine("Width: " + width);
+		Console.WriteLine("Area: " + FindArea());
+	}
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Rectangle r1 = new Rectangle();
+		Console.WriteLine("Rectangle 1 (Default Constructor)");
+		r1.Display();
+
+		Rectangle r2 = new Rectangle(10, 5);
+		Console.WriteLine("\nRectangle 2 (Parameterized Constructor)");
+		r2.Display();
+
+		Console.ReadLine();
+	}
+}
