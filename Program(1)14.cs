403623898f965aaf5d6e48803da25be803bcb536
kandregulapallavi <kandregula.ramkrishna127630@marwadiuniversity.ac.in>
2026-03-17 08:30:12 +0530

    Add files via upload

---
+++ Program(1)14.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Student
+{
+	int rollNo;
+	string name;
+	double marks;
+
+	public Student()
+	{
+		rollNo = 0;
+		name = "Unknown";
+		marks = 0;
+	}
+
+	public Student(int r, string n, double m)
+	{
+		rollNo = r;
+		name = n;
+		marks = m;
+	}
+
+	public string CalculateGrade()
+	{
+		if (marks >= 75)
+			return "A";
+		else if (marks >= 60)
+			return "B";
+		else if (marks >= 50)
+			return "C";
+		else
+			return "Fail";
+	}
+
+	public void Display()
+	{
+		Console.WriteLine("Roll No: " + rollNo);
+		Console.WriteLine("Name: " + name);
+		Console.WriteLine("Marks: " + marks);
+		Console.WriteLine("Grade: " + CalculateGrade());
+	}
+}
+
+
+class Program
+{
+	static void Main()
+	{
+		
+		Student s1 = new Student();
+		s1.Display();
+
+		Console.WriteLine();
+
+		Student s2 = new Student(1, "Pallavi", 82);
+		s2.Display();
+	}
+}
