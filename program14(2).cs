11e59736291eb0b75cea29e4f0ad61056606c928
kandregulapallavi <kandregula.ramkrishna127630@marwadiuniversity.ac.in>
2026-03-17 08:45:01 +0530

    Add files via upload

---
+++ program14(2).cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Employee
+{
+	int employeeId;
+	string name;
+	double salary;
+
+	public Employee()
+	{
+		employeeId = 0;
+		name = "Unknown";
+		salary = 0;
+	}
+
+	public Employee(int id, string n, double s)
+	{
+		employeeId = id;
+		name = n;
+		salary = s;
+	}
+
+	
+	public double CalculateAnnualSalary()
+	{
+		return salary * 12;
+	}
+	public void Display()
+	{
+		Console.WriteLine("Employee ID: " + employeeId);
+		Console.WriteLine("Name: " + name);
+		Console.WriteLine("Monthly Salary: " + salary);
+		Console.WriteLine("Annual Salary: " + CalculateAnnualSalary());
+	}
+}
+class Program
+{
+	static void Main()
+	{
+		Employee e1 = new Employee();
+		e1.Display();
+
+		Console.WriteLine();
+
+		
+		Employee e2 = new Employee(102, "Pallavi", 30000);
+		e2.Display();
+	}
+}
