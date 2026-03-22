45ecbe7cea600efabaa85ee6e1648ecf2e70eb3b
kandregulapallavi <kandregula.ramkrishna127630@marwadiuniversity.ac.in>
2026-03-22 16:28:58 +0530

    Add files via upload

---
+++ Program4(2).cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.Write("Enter Principal Amount: ");
+		double p = double.Parse(Console.ReadLine());
+
+		Console.Write("Enter Rate of Interest: ");
+		double r = double.Parse(Console.ReadLine());
+
+		Console.Write("Enter Time (in years): ");
+		double t = double.Parse(Console.ReadLine());
+
+		double si = (p * r * t) / 100;
+
+		Console.WriteLine("Simple Interest = " + si);
+	}
+}
