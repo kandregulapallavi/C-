23240c1a18c1e2f6e8782920b43d250cfa5dba7f
kandregulapallavi <kandregula.ramkrishna127630@marwadiuniversity.ac.in>
2026-03-29 11:30:38 +0530

    Add files via upload

---
+++ Program(7).cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class AgeCategory
+{
+	static void Main()
+	{
+		int age;
+
+		Console.Write("Enter your age: ");
+		age = Convert.ToInt32(Console.ReadLine());
+
+		if (age < 12)
+		{
+			Console.WriteLine("You are Kid");
+		}
+		else if (age >= 12 && age <= 17)
+		{
+			Console.WriteLine("You are Teenager");
+		}
+		else if (age >= 18 && age <= 60)
+		{
+			Console.WriteLine("You are Adult");
+		}
+		else
+		{
+			Console.WriteLine("You are Senior Citizen");
+		}
+	}
+}
---
+++ Program(9).cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class FibonacciProgram
+{
+	static void Main()
+	{
+		int n, a = 0, b = 1, c;
+
+		Console.Write("Enter the limit: ");
+		n = Convert.ToInt32(Console.ReadLine());
+
+		Console.Write("Fibonacci Series: " + a + " " + b + " ");
+
+		for (int i = 2; ; i++)
+		{
+			c = a + b;
+			if (c > n)
+				break;
+
+			Console.Write(c + " ");
+			a = b;
+			b = c;
+		}
+	}
+}
